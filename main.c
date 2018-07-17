141aed11424fb1860fa3dadb8b7e527a47011b94
jonkofee <jonkofee@jonkofees-MacBook-Pro.local>
2018-07-17 15:52:11 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include "wifi.h"
+
+homekit_characteristic_t current_temperature = HOMEKIT_CHARACTERISTIC_(
+    CURRENT_TEMPERATURE, 19
+);
+homekit_characteristic_t target_temperature  = HOMEKIT_CHARACTERISTIC_(
+    TARGET_TEMPERATURE, 22, .min_step = (float[]) {1}, .min_value = (float[]) {16}, .max_value = (float[]) {31}
+);
+homekit_characteristic_t units = HOMEKIT_CHARACTERISTIC_(TEMPERATURE_DISPLAY_UNITS, 0);
+homekit_characteristic_t current_state = HOMEKIT_CHARACTERISTIC_(CURRENT_HEATING_COOLING_STATE, 0);
+homekit_characteristic_t target_state = HOMEKIT_CHARACTERISTIC_(
+    TARGET_HEATING_COOLING_STATE, 0
+);
+homekit_characteristic_t cooling_threshold = HOMEKIT_CHARACTERISTIC_(
+    COOLING_THRESHOLD_TEMPERATURE, 25
+);
+homekit_characteristic_t heating_threshold = HOMEKIT_CHARACTERISTIC_(
+    HEATING_THRESHOLD_TEMPERATURE, 15
+);
+homekit_characteristic_t current_humidity = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+static void wifi_init() {
+    struct sdk_station_config wifi_config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASSWORD,
+    };
+
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&wifi_config);
+    sdk_wifi_station_connect();
+}
+
+void thermostat_identify(homekit_value_t _value) {
+    printf("Thermostat identify\n");
+}
+
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_thermostat, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Кондиционер"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "OASIS"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "001"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "undefined"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, thermostat_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(THERMOSTAT, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Кондиционер"),
+            &current_temperature,
+            &target_temperature,
+            &current_state,
+            &target_state,
+            &cooling_threshold,
+            &heating_threshold,
+            &units,
+            &current_humidity,
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+};
+
+void user_init(void) {
+    uart_set_baud(0, 115200);
+
+    wifi_init();
+    homekit_server_init(&config);
+}
