8e1ac232c104eb3bd4ce7e9dcd1203c9c936efad
jonkofee <jonkofee@icloud.com>
2019-05-27 12:17:16 +0300

    ⚡️ samsung old

--- marks/samsung/old/index.c
+++ marks/samsung/old/index.c
@@ -21,13 +21,52 @@ homekit_characteristic_t target_temperature = HOMEKIT_CHARACTERISTIC_(
 
 void setTemperature(int temperature)
 {
-  uint8_t sendBuffer[] = {0x4D, 0xB2, 0xD8, 0x00, 0x00, 0x00};
+  uint8_t sendBuffer[] = {0x4D, 0xB2, 0xFD, 0x00, 0x00, 0x00};
 
-  static const uint8_t temperatures[] = {0, 8, 12, 4, 6, 14, 10, 2, 3, 11, 9, 1, 5, 13, 7};
-
-  sendBuffer[2] |= CARRIER_AIRCON2_MODE_ON | CARRIER_AIRCON2_FAN_AUTO;
-
-  sendBuffer[4] |= CARRIER_AIRCON2_MODE_COOL | temperatures[(temperature - 17)];
+  switch (temperature)
+    {
+    case 17:
+      break;
+    case 18:
+      sendBuffer[4] = 0x08;
+      break;
+    case 19:
+      sendBuffer[4] = 0x0C;
+      break;
+    case 20:
+      sendBuffer[4] = 0x04;
+      break;
+    case 21:
+      sendBuffer[4] = 0x06;
+      break;
+    case 22:
+      sendBuffer[4] = 0x0E;
+      break;
+    case 23:
+      sendBuffer[4] = 0x0A;
+      break;
+    case 24:
+      sendBuffer[4] = 0x02;
+      break;
+    case 25:
+      sendBuffer[4] = 0x03;
+      break;
+    case 26:
+      sendBuffer[4] = 0x0B;
+      break;
+    case 27:
+      sendBuffer[4] = 0x09;
+      break;
+    case 28:
+      sendBuffer[4] = 0x01;
+      break;
+    case 29:
+      sendBuffer[4] = 0x05;
+      break;
+    case 30:
+      sendBuffer[4] = 0x0D;
+      break;
+    }
 
   // Checksums
   sendBuffer[3] = ~sendBuffer[2];
@@ -62,27 +101,32 @@ void setTemperature(int temperature)
 
 void off()
 {
-  printf("OFF");
-  static const uint8_t samsungOffCode[] = {0x01, 0x72, 0x0F, 0x00, 0x70, 0x41, 0x01, 0x01, 0xD2, 0xFE, 0x21, 0xD3, 0x15, 0xC0};
+    printf("OFF");
+    uint8_t sendBuffer[] = {0x4D, 0xB2, 0xDE, 0x21, 0x07, 0xF8};
 
-  // Header
-  ir_mark(2980);
-  ir_space(9240);
+    // Header
+    ir_mark(CARRIER_AIRCON2_HDR_MARK);
+    ir_space(CARRIER_AIRCON2_HDR_SPACE);
 
-  for (size_t i = 0; i < sizeof(samsungOffCode); i++)
-  {
-    sendIRbyte(samsungOffCode[i], 440, 520, 1548);
-
-    if (i == 6)
+    // Payload
+    for (size_t i = 0; i < sizeof(sendBuffer); i++)
     {
-      ir_mark(440);
-      ir_space(1548);
-      ir_mark(2980);
-      ir_space(9240);
+      sendIRbyte(sendBuffer[i], CARRIER_AIRCON2_BIT_MARK, CARRIER_AIRCON2_ZERO_SPACE, CARRIER_AIRCON2_ONE_SPACE);
     }
-  }
 
-  // End mark
-  ir_mark(440);
-  ir_space(0);
+    // New header
+    ir_mark(CARRIER_AIRCON2_BIT_MARK);
+    ir_space(CARRIER_AIRCON2_HDR_SPACE);
+    ir_mark(CARRIER_AIRCON2_HDR_MARK);
+    ir_space(CARRIER_AIRCON2_HDR_SPACE);
+
+    // Payload again
+    for (size_t i = 0; i < sizeof(sendBuffer); i++)
+    {
+      sendIRbyte(sendBuffer[i], CARRIER_AIRCON2_BIT_MARK, CARRIER_AIRCON2_ZERO_SPACE, CARRIER_AIRCON2_ONE_SPACE);
+    }
+
+    // End mark
+    ir_mark(CARRIER_AIRCON2_BIT_MARK);
+    ir_space(0);
 }
